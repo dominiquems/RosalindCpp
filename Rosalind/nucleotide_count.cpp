55c75b994639b7a7835c53efa5236bc80df318ca
Dominique <dominique.massau@gmail.com>
2020-09-23 16:55:05 +0200

    Create function readFile

--- Rosalind/nucleotide_count.cpp
+++ Rosalind/nucleotide_count.cpp
@@ -1,12 +1,26 @@
-#include<iostream>
 #include<boost/algorithm/string.hpp>
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+/**
+* @brief A function that reads and returns a line from a text file
+* 
+* @param: fileName, name of the text file to read from
+* @return: string, represents a DNA sequence
+**/
+std::string readFile(std::string fileName)
+{
+    std::ifstream infile(fileName);
+    std::string line{};
+
+    std::getline(infile, line);
+    return line;
+}
 
 
 int main()
 {
-    std::string userName{ "Dominique" };
-    std::string prefix{ "Pomin" };
-
-    std::cout << boost::algorithm::starts_with(userName, prefix);
+    readFile("test.txt");
 }
