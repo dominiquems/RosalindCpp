eca2bf5e1768fa5fd78f09ae95f416be54aee110
Dominique <dominique.massau@gmail.com>
2020-09-23 19:22:09 +0200

    Create function isValidSeq

--- Rosalind/nucleotide_count.cpp
+++ Rosalind/nucleotide_count.cpp
@@ -1,9 +1,9 @@
+#include <boost/algorithm/string/erase.hpp>
 #include <boost/format.hpp>
-
+#include <regex>
 #include <map>
 #include <iostream>
 #include <fstream>
-#include <sstream>
 
 /**
 * @brief A function that reads and returns a line from a text file
@@ -17,6 +17,7 @@ std::string readFile(std::string fileName)
     std::string line{};
 
     std::getline(infile, line);
+    boost::erase_all(line, "\n");
     return line;
 }
 
@@ -39,17 +40,42 @@ std::map<char, int> countNucleotide(std::string dnaSeq)
         else
         {
             ++nucleotideDict[dnaSeq[i]];
-            std::cout << dnaSeq[i] << '\n';
         }
     }
     return nucleotideDict;
 }
 
+/**
+* @brief Check if only nucleotide symbols are present in the DNA sequence
+* 
+* @param dnaSeq: str, represents a DNA sequence
+* @return: bool, 1 if the sequence is valid and 0 otherwise
+**/
+bool isValidSeq(std::string dnaSeq)
+{
+    std::regex pattern("[ACGT]*");
+
+    if (std::regex_match(dnaSeq, pattern))
+    {
+        return 1;
+    }
+    else
+    {
+        std::cout << "Sequence contains unknown characters\n";
+        return 0;
+    }
+}
 
 int main()
 {
     std::string dnaSeq{ readFile("test.txt") };
+    if (!isValidSeq(dnaSeq))
+    {
+        std::cout << "Halting...\n";
+        return -1;
+    }
     std::map<char, int> nucleotideDict{ countNucleotide(dnaSeq) };
-    std::cout << boost::format("%1% %2% %3% %4%") % nucleotideDict['A'] % nucleotideDict['C'] % 
-        nucleotideDict['G'] % nucleotideDict['T'];
+    std::cout << boost::format("%1% %2% %3% %4%") % nucleotideDict['A'] % 
+        nucleotideDict['C'] % nucleotideDict['G'] % nucleotideDict['T'];
+    return 0;
 }
